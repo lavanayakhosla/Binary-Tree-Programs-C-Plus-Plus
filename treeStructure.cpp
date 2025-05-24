49c854a94746bc93f75df8d2300d1e28af4313db
lavanayakhosla <144594627+lavanayakhosla@users.noreply.github.com>
2025-05-24 18:12:58 +0530

    Create treeStructure.cpp

---
+++ treeStructure.cpp
@@ -0,0 +1,31 @@
+// This is the program on how to define the structure of a tree in CPP
+// I created a structure (custom data type)  called Node
+// I have three data members - data (to store the values of each node in the tree) , a pointer of type struct Node , which points to the left child and another point of same type to point to right child.
+// I have created a constructor called Node , the constructor is a parametrised constructor that takes argument val , 
+// the val argument passed to Node constructor will store val in data, left and right are initialised as NULL pointers
+// in the main function first I did dynamic memory allocation using new keyword and stored whatever value i wanted to pass through the constructor. root is the pointer that stores the address of the new Node (1)
+
+
+#include<iostream>
+using namespace std;
+struct Node {
+    int data;
+    struct Node * left;
+    struct Node * right;
+    Node(int val){
+        data=val;
+        left = right = NULL;
+
+    }
+};
+int main(){
+    struct Node * root = new Node (1);
+    root -> left = new Node (2);
+    root -> right = new Node(3);
+    root -> left ->right= new Node(5);
+    cout<<(*root).data;
+    cout<< root->left->data;
+    cout<< root->left->right->data;
+    cout<<root->data;
+    
+}
